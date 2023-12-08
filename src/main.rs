7200f400de92136efa94b2dace2cf882b6cbb44f
Minh Nguyen <luonguyen@microsoft.com>
2023-12-08 01:08:05 -0500

    Added Value and finished parsing
    Signed-off-by: Minh Nguyen <luonguyen@microsoft.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,41 @@
-use std::{fs, env};
+use std::{fs, fmt, env};
 use std::str::Chars;
 use std::iter::Peekable;
-use anyhow::{Result, Error};
+use std::collections::HashMap;
+use anyhow::Result;
 
 struct Parser<'a> {
     input: Peekable<Chars<'a>>,
+}
+
+enum Value {
+    Nil,
+    Bool(bool),
+    Int(i64),
+    Float(f64),
+    String(String),
+    Symbol(String),
+    List(Vec<Value>),
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Value::Nil => write!(f, "NIL"),
+            Value::Bool(b) => write!(f, "{}", b),
+            Value::Int(i) => write!(f, "{}", i),
+            Value::Float(fl) => write!(f, "{}", fl),
+            Value::String(s) => write!(f, "{}", s),
+            Value::Symbol(s) => write!(f, "{}", s),
+            Value::List(lst) => {
+                write!(f, "(")?;
+                for val in lst {
+                    write!(f, "{} ", val)?;
+                }
+                write!(f, ")")
+            }
+        }
+    }
 }
 
 impl<'a> Parser<'a> {
@@ -14,37 +45,95 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn parse_list(&mut self) -> Result<(), Error> {
-        self.input.next();
-        println!("<list>");
+    fn parse_list(&mut self) -> Value {
+        if let Some(&ch) = self.input.peek() {
+            assert_eq!(ch, '(');
+            self.input.next();
+        } else {
+            panic!("No more characters!");
+        }
+        let mut values: Vec<Value> = Vec::new();
+        let mut quoted = false;
         while let Some(&c) = self.input.peek() {
-            if c == ')' {
-                self.input.next();
-                break;
-            } else if c == '(' {
-                let _ = self.parse_list();
-            } else if c.is_whitespace() {
-                self.input.next();
-            } else {
-                let _ = self.parse_atom();
+            match c {
+                ')' => {
+                    self.input.next();
+                    break;
+                }
+                '\'' => {
+                    quoted = true;
+                    self.input.next();
+                }
+                '(' => {
+                    let value = self.parse_list();
+                    if quoted {
+                        quoted = false;
+                        let vec = vec![Value::Symbol("quote".to_string()), value];
+                        values.push(Value::List(vec));
+                    } else {
+                        values.push(value);
+                    }
+                }
+                _ if c.is_whitespace() => {
+                    self.input.next();
+                }
+                _ => {
+                    let value = self.parse_atom();
+                    if quoted {
+                        quoted = false;
+                        let vec = vec![Value::Symbol("quote".to_string()), value];
+                        values.push(Value::List(vec));
+                    } else {
+                        values.push(value);
+                    }
+                }
             }
         }
-        println!("</list>");
-        Ok(())
+        Value::List(values)
     }
 
-    fn parse_atom(&mut self) -> Result<(), Error> {
+    fn parse_atom(&mut self) -> Value {
         let mut atom = String::new();
+        let mut first_char = '\0';
         while let Some(&c) = self.input.peek() {
-            if c.is_whitespace() || c == ')' {
-                break;
-            } else {
-                atom.push(c);
+            match c {
+                _ if c.is_whitespace() || c == ')' => {
+                    break;
+                }
+                _ => {
+                    if first_char == '\0' {
+                        first_char = c;
+                    }
+                    atom.push(c);
+                }
             }
             self.input.next();
         }
-        println!("{}", atom);
-        Ok(())
+        if atom.is_empty() {
+            return Value::Nil;
+        }
+        atom = atom.to_uppercase();
+        match first_char {
+            '\"' => {
+                Value::String(atom)
+            }
+            _ if atom == "T" => {
+                Value::Bool(true)
+            }
+            _ if atom == "NIL" => {
+                Value::Bool(false)
+            }
+            _ if first_char.is_digit(10) => {
+                if atom.contains('.') {
+                    Value::Float(atom.parse::<f64>().unwrap())
+                } else {
+                    Value::Int(atom.parse::<i64>().unwrap())
+                }
+            }
+            _ => {
+                Value::Symbol(atom)
+            }
+        }
     }
 }
 
@@ -60,10 +149,15 @@ fn main() -> Result<()> {
     let lisp_source: String = fs::read_to_string(&args[1])?;
     let mut parser = Parser::new(&lisp_source);
 
-    // Parse the lisp source into s-expressions
-    match parser.parse_list() {
-        Ok(_) => println!("Parsing completed!"),
-        Err(e) => println!("An error occurred: {:?}", e),
+    while let Some(&c) = parser.input.peek() {
+        match c {
+            '(' => {
+                println!("{}", parser.parse_list());
+            }
+            _ => {
+                parser.input.next();
+            }
+        }
     }
 
     Ok(())
--- tests/test1.lisp
+++ tests/test1.lisp
@@ -1,15 +1,20 @@
-;; Define the factorial function
 (defun factorial (n)
+  (if (<= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+(defun factorial_loop (n)
   (let ((result 1))
     (do ((i 1 (+ i 1)))
         ((> i n) result)
       (setf result (* result i)))))
 
-;; Define the choose function
 (defun choose (n k)
-  (/ (factorial n)
-     (* (factorial k) (factorial (- n k)))))
+  (/ (factorial_loop n)
+     (* (factorial_loop k) (factorial_loop (- n k)))))
 
-;; Calculate choose(12 7)
 (let ((result (choose 12 7)))
-  (format t "~A~%" result))
+  (write result))
+
+(let ((assert_eq (= (factorial 11) (factorial_loop 11))))
+  (write assert_eq))
